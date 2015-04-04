5a0002af7018379a466dffc9db6e4fc7d50608d5
jelshreds72 <jelshredsbb@gmail.com>
2015-04-04 15:47:32 +0000

    Automatic commit

--- random.cpp
+++ random.cpp
@@ -7,26 +7,34 @@ int main(void){
 
 	srand( time( NULL) ); // seed random number from time
 	int random_number = 1 + (rand() % 10); // random number between 1 and 10
-	// declare variable to store guess of user and set to 0
-	// declare variable as counter for number of guesses
+ int guess = 0;	// declare variable to store guess of user and set to 0
+ int counter = 0;	// declare variable as counter for number of guesses
 
 	
 	for(;;)
 	{
-		// increment guess counter
-
-		// read input from user
+	counter = counter + 1;	// increment guess counter
+   //	cout << counter << endl; 
+	cin >> guess;	// read input from user
 		//
 		
 		// compare input from user to random number
 		// print out winner, too high or too low message
 		//
 
-	}
+		if (guess > random_number){cout << "too high\n";
+		
+		} else if (guess < random_number){cout << "too low\n";
+		}
+		
+		 else{cout << "winner\n"; break;
+		 	
+		 }
 
 	// print message with number of guesses
-	//
+	// 
 	
-
+}
 	return 0;
 }
+ 
